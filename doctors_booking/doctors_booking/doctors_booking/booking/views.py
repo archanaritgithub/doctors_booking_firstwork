c893639a50549282fe1ec86ba0ebf18237b35784
archana <archanakrishna202@gmail.com>
2024-03-19 12:47:50 +0530

    work

---
+++ doctors_booking/doctors_booking/doctors_booking/booking/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Departments, Doctors
+from . forms import BookingForm
+
+# Create your views here.
+def index(requset):
+    return render(requset, 'index.html')
+
+def about(requset):
+    return render(requset, 'about.html')
+
+def booking(request):
+    if request.method == "POST":
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'conformation.html')
+    form = BookingForm()
+    dict_form={
+        'form': form
+    }
+    return render(request, 'booking.html', dict_form)
+        
+
+def doctors(request):
+    dict_docs = {
+        'doctors': Doctors.objects.all()
+    }
+    return render(request, 'doctors.html', dict_docs)
+
+def contact(requset):
+  return render(requset, 'contact.html')
+
+def department(request):
+    dict_dept={
+        'dept': Departments.objects.all()
+    }
+    return render(request, 'department.html', dict_dept)
